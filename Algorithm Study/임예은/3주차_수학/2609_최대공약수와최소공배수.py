6e0aa2a7d70631dbabcf1ba19fe35b0b55ef8f89
yeeunlim <84782741+yeeunlim@users.noreply.github.com>
2024-04-09 15:24:33 +0900

    수학

---
+++ Algorithm Study/임예은/3주차_수학/2609_최대공약수와최소공배수.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('2609.txt', 'r')
+a, b = map(int, input().split())
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+print(gcd(a, b))
+print(lcm(a, b))
